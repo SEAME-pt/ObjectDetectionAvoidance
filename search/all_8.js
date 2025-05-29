47901aa118cac4e929c655c05c7a84b105d5864c
matde-je <matildecosta719@gmail.com>
2025-05-29 19:17:40 +0000

    Deploying to gh-pages from @ SEAME-pt/ObjectDetectionAvoidance@ee4c716cb9ed8fb0ed1e16ee25394aadc933d379 🚀

--- search/all_8.js
+++ search/all_8.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['altper54_5fjpg_2erf_2ef98de7009197f640f0facc0fcafddcad_2ejpg_0',['altper54_jpg.rf.f98de7009197f640f0facc0fcafddcad.jpg',['../predict_2altper54__jpg_8rf_8f98de7009197f640f0facc0fcafddcad_8jpg.html',1,'(Global Namespace)'],['../predict2_2altper54__jpg_8rf_8f98de7009197f640f0facc0fcafddcad_8jpg.html',1,'(Global Namespace)'],['../predict32_2altper54__jpg_8rf_8f98de7009197f640f0facc0fcafddcad_8jpg.html',1,'(Global Namespace)'],['../predict33_2altper54__jpg_8rf_8f98de7009197f640f0facc0fcafddcad_8jpg.html',1,'(Global Namespace)'],['../predict34_2altper54__jpg_8rf_8f98de7009197f640f0facc0fcafddcad_8jpg.html',1,'(Global Namespace)'],['../predict35_2altper54__jpg_8rf_8f98de7009197f640f0facc0fcafddcad_8jpg.html',1,'(Global Namespace)'],['../predict36_2altper54__jpg_8rf_8f98de7009197f640f0facc0fcafddcad_8jpg.html',1,'(Global Namespace)']]],
   ['and_20avoidance_1',['Object Detection and Avoidance',['../md__2home_2runner_2work_2ObjectDetectionAvoidance_2ObjectDetectionAvoidance_2README.html',1,'']]],
-  ['and_20testing_2',['and testing',['../index.html#autotoc_md5',1,'Training and Testing'],['../md__2home_2runner_2work_2ObjectDetectionAvoidance_2ObjectDetectionAvoidance_2README.html#autotoc_md12',1,'Training and Testing']]],
-  ['annotations_3',['annotations',['../index.html#autotoc_md4',1,'Creating Annotations'],['../md__2home_2runner_2work_2ObjectDetectionAvoidance_2ObjectDetectionAvoidance_2README.html#autotoc_md11',1,'Creating Annotations']]],
+  ['and_20testing_2',['and testing',['../index.html#autotoc_md6',1,'Training and Testing'],['../md__2home_2runner_2work_2ObjectDetectionAvoidance_2ObjectDetectionAvoidance_2README.html#autotoc_md13',1,'Training and Testing']]],
+  ['annotations_3',['annotations',['../index.html#autotoc_md5',1,'Creating Annotations'],['../md__2home_2runner_2work_2ObjectDetectionAvoidance_2ObjectDetectionAvoidance_2README.html#autotoc_md12',1,'Creating Annotations']]],
   ['architecture_4',['Project Architecture',['../index.html',1,'']]],
   ['avoidance_5',['Object Detection and Avoidance',['../md__2home_2runner_2work_2ObjectDetectionAvoidance_2ObjectDetectionAvoidance_2README.html',1,'']]]
 ];
--- search/all_a.js
+++ search/all_a.js
@@ -8,7 +8,7 @@ var searchData=
   ['copy_5fdirectory_5ffiles_5',['copy_directory_files',['../namespacecopy__files.html#a729c9559d3b5a485ee1f351cea8d5a15',1,'copy_files']]],
   ['copy_5ffiles_6',['copy_files',['../namespacecopy__files.html',1,'copy_files'],['../namespacetraining.html#afec4c2a797c7e0adec7cade809245dea',1,'training.copy_files()']]],
   ['copy_5ffiles_2epy_7',['copy_files.py',['../copy__files_8py.html',1,'']]],
-  ['copy_5fmarkdown_5flines_8',['copy_markdown_lines',['../namespacecopy__readme.html#a6fa1efffc51ec08cb2e8cd584b7543aa',1,'copy_readme']]],
+  ['copy_5fmarkdown_5flines_8',['copy_markdown_lines',['../namespacecopy__readme.html#a701d9b522b97d15ee94cad906d18a9c4',1,'copy_readme']]],
   ['copy_5fmatching_5ffiles_9',['copy_matching_files',['../namespacecopy__files.html#a98cfd5c60991b6c019447d5c2e7577e6',1,'copy_files']]],
   ['copy_5freadme_10',['copy_readme',['../namespacecopy__readme.html',1,'']]],
   ['copy_5freadme_2epy_11',['copy_readme.py',['../copy__readme_8py.html',1,'']]],
@@ -17,5 +17,5 @@ var searchData=
   ['count_5flabels_2epy_14',['count_labels.py',['../count__labels_8py.html',1,'']]],
   ['create_5fannotations_15',['create_annotations',['../namespacecreate__annotations.html',1,'']]],
   ['create_5fannotations_2epy_16',['create_annotations.py',['../create__annotations_8py.html',1,'']]],
-  ['creating_20annotations_17',['creating annotations',['../index.html#autotoc_md4',1,'Creating Annotations'],['../md__2home_2runner_2work_2ObjectDetectionAvoidance_2ObjectDetectionAvoidance_2README.html#autotoc_md11',1,'Creating Annotations']]]
+  ['creating_20annotations_17',['creating annotations',['../index.html#autotoc_md5',1,'Creating Annotations'],['../md__2home_2runner_2work_2ObjectDetectionAvoidance_2ObjectDetectionAvoidance_2README.html#autotoc_md12',1,'Creating Annotations']]]
 ];
