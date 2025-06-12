ff9f41198918cd525fdcb2fe676bd52a5a280bb4
matde-je <matildecosta719@gmail.com>
2025-06-12 11:49:11 +0100

    deleting docs

--- doxyfiles/docs/search/variables_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['label_5fdirectory_11850',['label_directory',['../namespacebbox__seg.html#a118fd44307a18d1bf4d3ab68f914da32',1,'bbox_seg.label_directory()'],['../namespacecount__labels.html#a4793950b2b52179cb98939d0812aba96',1,'count_labels.label_directory()'],['../namespacedelete__images.html#af5fae933531d86b54c4bb1da6237d178',1,'delete_images.label_directory()']]]
-];
--- doxyfiles/docs/search/variables_b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['val_5fratio_11864',['val_ratio',['../namespaceseparate__mask__img.html#ae313e3221dbec5b78d6ecfd77efbdfc9',1,'separate_mask_img']]]
-];
